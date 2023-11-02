e18b311d8432fa189bed1c6bc87d0901c3973022
Enoc Balderas <balderasenoc@gmail.com>
2023-11-02 16:05:32 -0700

    optimize solution by using hashing instead of brute force

--- string/find_border.cpp
+++ string/find_border.cpp
@@ -1,7 +1,69 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 typedef unsigned int uint;
+
+void solve(const string& str);
+void solve_using_hashing(const string& str);
+
+void solve(const string& str)
+{
+    for(auto itr = str.begin(); itr != str.end(); ++itr)
+    {
+        string prefix = str.substr(0, distance(str.begin(), itr));
+
+        if(prefix.size() != str.size() && prefix.size() != 0)
+        {
+            if(equal(str.begin() + (str.size() - prefix.size()), str.end(), prefix.begin()))
+            {
+                cout << prefix.size() << endl;
+            }
+        }
+    }
+}
+
+void solve_using_hashing(const string& str)
+{
+    const int p = 31;
+    const int m = 1e9 + 9;
+    const int s_size = str.size();
+    vector<long long> p_pow(s_size);
+
+    //preprocessing
+    //calculate powers of p
+    p_pow[0] = 1;
+    for(int i = 1; i < s_size; ++i)
+    {
+        p_pow[i] = (p_pow[i - 1]*p) % m;
+    }
+
+    //calculate hash of prefix str
+    vector<long long> s_hash(s_size + 1, 0);
+    for(int i = 0; i < s_size; ++i)
+    {
+        s_hash[i + 1] = (s_hash[i] + (str[i] - 'a' + 1)*p_pow[i]) % m;
+    }
+
+    //find borders
+    long long pre_hash;
+    long long suf_hash;
+    for(int i = 0; i < s_size - 1; ++i)
+    {
+        pre_hash = s_hash[i + 1];
+        suf_hash = (s_hash[s_size] + m - s_hash[s_size - (i + 1)]) % m;
+
+        if((pre_hash*p_pow[s_size - (i + 1)]) % m == suf_hash)
+            cout << i + 1 << endl;
+
+        /*
+        cout << "pre_hash: " << (pre_hash*p_pow[s_size - (i + 1)]) % m << endl;
+        cout << "suf_hash: " << suf_hash << endl;
+        */
+    }
+}
 
 int main()
 {
@@ -18,18 +80,7 @@ int main()
 
     cin >> str;
 
-    for(auto itr = str.begin(); itr != str.end(); ++itr)
-    {
-        string prefix = str.substr(0, distance(str.begin(), itr));
-
-        if(prefix.size() != str.size() && prefix.size() != 0)
-        {
-            if(equal(str.begin() + (str.size() - prefix.size()), str.end(), prefix.begin()))
-            {
-                cout << prefix.size() << endl;
-            }
-        }
-    }
+    solve_using_hashing(str);
 
     return 0;
 }
--- string/string_match.cpp
+++ string/string_match.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 typedef unsigned int uint;
+
+int solve(const string& str, const string& pattern);
+int rabin_karp(const string& haystack, const string& needle);
+
+int rabin_karp(const string& haystack, const string& needle)
+{
+    const int p = 31;
+    const int m = 1e9 + 9;
+    const int h_size = haystack.size();
+    const int n_size = needle.size();
+    vector<long long> p_pow(h_size);
+
+    //preprocessing
+    //calculate powers of p
+    p_pow[0] = 1;
+    for(int i = 1; i < h_size; ++i)
+    {
+        p_pow[i] = (p_pow[i - 1]*p) % m;
+    }
+
+    //calculate hash of prefix of haystack
+    vector<long long> h_hash(h_size + 1, 0);
+    for(int i = 0; i < h_size; ++i)
+    {
+        h_hash[i + 1] = (h_hash[i] + (haystack[i] - 'a' + 1)*p_pow[i]) % m;
+    }
+
+    //calculate hash of needle
+    long long n_hash = 0;
+    for(int i = 0; i < n_size; ++i)
+    {
+        n_hash = (n_hash + (needle[i] - 'a' + 1)*p_pow[i]) % m;
+    }
+
+    //count occurances
+    int count = 0;
+    for(int i = 0; i + n_size - 1 < h_size; ++i)
+    {
+        long long substr_hash = (h_hash[i + n_size] + m - h_hash[i]) % m;
+
+        if(substr_hash == ((n_hash*p_pow[i]) % m))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int solve(const string& str, const string& pattern)
+{
+    int count = 0;
+
+    for(auto itr = str.begin(); itr != str.end(); ++itr)
+    {
+        string substr = str.substr(distance(str.begin(), itr), pattern.size());
+
+        if(pattern == substr)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 int main()
 {
@@ -22,15 +88,7 @@ int main()
     cin >> str;
     cin >> pattern;
 
-    for(auto itr = str.begin(); itr != str.end(); ++itr)
-    {
-        string substr = str.substr(distance(str.begin(), itr), pattern.size());
-
-        if(pattern == substr)
-        {
-            count++;
-        }
-    }
+    count = rabin_karp(str, pattern);
 
     cout << str << "\n";
     cout << pattern << "\n";
